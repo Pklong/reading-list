9f104872cf1affdf7895780461494d1c24a1d04a
PKLong <patrickklong@gmail.com>
2017-09-20 20:50:26 -0400

    Update config for Api Gateway SDK

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ const config = {
   IdentityPoolId: 'us-east-1:dfd69685-4db9-42fb-bffa-7cc0f411a7dd',
   UserPoolId: 'us-east-1_Lk6m7odJJ',
   ClientId: '1n5f7ae8cg4sdva96k6qj5oa5t',
-  loginKey: 'cognito-idp.us-east-1.amazonaws.com/us-east-1_Lk6m7odJJ'
+  loginKey: 'cognito-idp.us-east-1.amazonaws.com/us-east-1_Lk6m7odJJ',
+  invokeUrl: 'https://twkuid58n9.execute-api.us-east-1.amazonaws.com/dev'
 }
 
 export default config
